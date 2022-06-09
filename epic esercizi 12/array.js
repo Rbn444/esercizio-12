6e34f2ee7965a06cd82bd0b70d5ee9a20cb45aab
Rbn444 <106075599+Rbn444@users.noreply.github.com>
2022-06-09 16:55:17 +0200

    Add files via upload

---
+++ epic esercizi 12/array.js
@@ -0,0 +1,43 @@
+let persone = ["pino", "gino", "lino"];
+console.log(persone);
+let terzo = persone [0];
+console.log(terzo);
+
+
+let text1 = "questa è una frase di prova";
+const myArray = text1.split(" " , 2);
+console.log(myArray);
+
+const arr1 = ["questa"];
+const arr2 = ["è"];
+const conc = arr1.concat(arr2);                         
+console.log(conc);
+
+let text = "ecco un testo maiuscolo";
+let result = text.toUpperCase();
+console.log(result);
+
+let length = text.length;
+console.log(length);
+
+persone.push ("dino");
+console.log(persone);
+
+persone.pop();
+console.log(persone);
+
+persone.shift();
+console.log(persone);
+
+persone.unshift("tino");
+console.log(persone);
+
+let matrice = [[21, 22, 23], [31, 32, 33], [41, 42, 43]];
+let trentadue = matrice [1] [1];
+console.log(trentadue);
+
+
+
+
+
+
